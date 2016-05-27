212b0f7e80da32cde205bf90dc8a372ae62cf977
Steve Kim <hh963103@gmail.com>
2016-05-27 18:35:00 +0900

    pod reinstall

--- Example/Pods/Target Support Files/SQLiteMapper/SQLiteMapper-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SQLiteMapper : NSObject
-@end
-@implementation PodsDummy_SQLiteMapper
-@end
--- Example/Pods/Target Support Files/SQLiteMapper/SQLiteMapper-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double SQLiteMapperVersionNumber;
-FOUNDATION_EXPORT const unsigned char SQLiteMapperVersionString[];
-
